4406a39a2c81acc2b2bee6656c43aac83d48e9f3
七Mi <1121734772@qq.com>
2022-03-02 10:48:15 +0800

    更新华住会签到

--- qd_hzh.py
+++ qd_hzh.py
@@ -6,79 +6,43 @@ cron:
 0 7 * * * qd_hzh.py
 """
 
+
+
 import requests
 
-cookies = {
-    'Cookie:userToken': 'ed667ecdc3e5486da5733afa33b6743e139581209',
-    'Domain': '.huazhu.com',
-    'Path': '/',
-    'expires': 'Thu Mar 31 13:55:28 GMT+08:00 2022',
-    '_hudVID': '7549a502-aa81-ee55-1e77-776481c65c07',
-    '_hudPVID': '2',
-    '_hudSID_TS': '1646114130009',
-    '_hudSID': '1646114130009_1',
-    '_hudSource': '',
-    'gr_user_id': 'f45c2a40-c6c6-4315-8e1f-29dac60481dc',
-    'gr_session_id_8f6e3e7f89d647cab9784afa81ea87bd': '207cf5eb-3f04-4d10-bb76-b660e0d3a477',
-    'gr_session_id_8f6e3e7f89d647cab9784afa81ea87bd_207cf5eb-3f04-4d10-bb76-b660e0d3a477': 'true',
-    'SK': 'ed667ecdc3e5486da5733afa33b6743e139581209',
-    'ec': 'oMErIu33-1646114130746-158c8a2f07534-1350139280',
-    '_efmdata': 'pEUR9y6IbxkDQbBGWSFhgxWrrODd0qOaFAtPjIlUP0kVD85ACofUtW6EANz9LLgZ3YiKkBCg3DmItqvBznXd8S6lXBSBi0b21CPrGz1YQzI%3D',
-    '_exid': 'zQHbfVN4FH3x56DrpKNuDSoPygs%2FAQQDpMguv54Ijb1TqZGKEFR61IjKGALiRnRoJ%2BCCw19pB%2FbRRhOWk9QNzQ%3D%3D',
+import time, datetime
+t=int(round(time.time() * 1000))
+headers = {
+    'Connection': 'keep-alive',
+    'sec-ch-ua': '""',
+    'DNT': '1',
+    'sec-ch-ua-mobile': '?1',
+    'User-Agent': 'Mozilla/5.0 (Linux; U; Android 11; zh-cn; M2007J1SC Build/RKQ1.200826.002) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.116 Mobile Safari/537.36 XiaoMi/MiuiBrowser/15.7.22 app/vipaccount Edg/98.0.4758.102',
+    'sec-ch-ua-platform': '',
+    'Accept': '*/*',
+    'Origin': 'https://campaign.huazhu.com',
+    'Sec-Fetch-Site': 'cross-site',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://campaign.huazhu.com/',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
 }
 
+params = (
+    ('stm', t),
+)
 
-def sign():
-    headers = {
-        'Host': 'hweb-mbf.huazhu.com',
-        'Connection': 'keep-alive',
-        'Content-Length': '14',
-        'Pragma': 'no-cache',
-        'Cache-Control': 'no-cache',
-        'Accept': 'application/json, text/plain, */*',
-        'Client-Platform': 'APP-ANDROID',
-        'User-Agent': 'HUAZHU/android/M2012K10C/12/8.6.2/Mozilla/5.0 (Linux; Android 12; M2012K10C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/95.0.4638.74 Mobile Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Origin': 'https://campaign.huazhu.com',
-        'X-Requested-With': 'com.htinns',
-        'Sec-Fetch-Site': 'same-site',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://campaign.huazhu.com/',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-    }
+data = {
+  '6\x86\xF0D\n\xE6\x05\xC6\b`#\0\xB0\x01\x80f()\x92\x0B@\x0E\x01\x98\b\t\x87%\x88\x13\x80c|\b\r\x80F\x1C\f\xDD\0L\x13@V$*\xEB\xB0\0h\xC0\x06q\x86\x13\x176\x15\x88\x13\xC7\x07\x15\x04\\\x11\x95e\xC7\x02>ik`\xA4\x88\xB6$\x03\x84\x01w\x15@\r\x95\0\xD6B\xC2\x98\x0Bc\x01\x9D$\x8C\xF1\xE3\xA0\x1D\x9B\xEC\xAE\xC2\0\x0EN.n\x1E^\fx\xC4\xDE(\xC2l\x16p\x8E\x81p\0\x96\0\xE6\0v\0t\0\x16\x10p\0^yYT\0\xF6\xCEA\xE2\0\xF4\x81\xA5)\x19\xA6"\0\xCA9\xA5\x81U\xF5l\x98\0\x1E\xB9N\x96\xF6\x980\xA0`9\xE29\xA6\xA6\x81"\xD0UUT\x89\xC9\xE9\xD9y\x85\xC5e\x8E5u\r\xCD\xAD\xED"\xA6p\xA6)TU\x8E\x98l)pg\x98\x19\x108\fY\xE4H\xC8\x04phY\x81\x19i\xF6\0n\xE2\xE7;\x83\xC9\xECAx\x19\xDE\x9Fo\xBD\x9B\xADT\xBA\xFC\0\xC4\0\'R\xA9T\xC5S\x85d\xE0\x81v\x869': '',
+  '\x94\xC2|\xE0\xF8\x9C \\\xE5\x91\x12\x05,)4F$EFFY,\0wDV': '\x12\x95\xF8\x95J\r[\x8D#\x95\x90BYJ\xB6,\x86N\x0B\xF7I\x1CR\xDC\xF6g0\x11\x02\xAB\\1\xD4\xCC',
+  '\xD9X\xE3Ht5`\0/\xA0\x9C\x03\r\x82\xCA\x91(\xBEg \x13\x8F\xE5\xE2\t\xD6\x92y@\x91J\xA5\x9B\xC9\xF4\xD2\xA3%\x96\xC8\xC5\x94y\rY@\xA8R \x14J\xD2R\x99F>X\xAF\xB1\x8D`\xF0\xAA\x88\x99`\x04\x90\xC9\xFD\xC4\xB1\xF8\xE6I;\xA8\x02\xE8\xEA\xB3@\0': ''
+}
 
-    data = {
-        'state': '1',
-        'day': '01'
-    }
+response = requests.post('https://api.growingio.com/v2/8f6e3e7f89d647cab9784afa81ea87bd/web/action', headers=headers, params=params, data=data)
 
-    response = requests.post('https://hweb-mbf.huazhu.com/api/signIn', headers=headers, cookies=cookies, data=data)
-    print(response.text)
+# Note: original query string below. It seems impossible to parse and
+# reproduce query strings 100% accurately so the one below is given
+# in case the reproduced version is not "correct".
+#response = requests.post('https://api.growingio.com/v2/8f6e3e7f89d647cab9784afa81ea87bd/web/action?stm=1646188677235', headers=headers, data=data)
+print(response)
 
-def sign1():
-    headers = {
-        'Host': 'hweb-mbf.huazhu.com',
-        'Connection': 'keep-alive',
-        'Pragma': 'no-cache',
-        'Cache-Control': 'no-cache',
-        'Accept': 'application/json, text/plain, */*',
-        'Client-Platform': 'APP-ANDROID',
-        'User-Agent': 'HUAZHU/android/M2012K10C/12/8.6.2/Mozilla/5.0 (Linux; Android 12; M2012K10C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/95.0.4638.74 Mobile Safari/537.36',
-        'Origin': 'https://campaign.huazhu.com',
-        'X-Requested-With': 'com.htinns',
-        'Sec-Fetch-Site': 'same-site',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://campaign.huazhu.com/',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-    }
-
-    response1 = requests.get('https://hweb-mbf.huazhu.com/api/singInIndex', headers=headers, cookies=cookies)
-    print(response1.text)
-
-if __name__=='__main__':
-    sign()
-    sign1()
