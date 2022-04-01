966671b9c191d39fb6f8d3f68924284ac084b48d
七Mi <1121734772@qq.com>
2022-04-01 11:02:04 +0800

    更新miui历史签到

--- qd_miui_history.py
+++ qd_miui_history.py
@@ -10,31 +10,40 @@ cron:
 
 import requests
 
+cookies = {
+    'wordpress_sec_f82bae77b4519a305f00e3f66136abf7': 'zq1121734772%7C1649991583%7CfV9buiUC9OrWiuxM2dUzyH4DYG5soU3p8xL8yqGZRgn%7C2566c6da3e2145278cf58137313b609e78281e756268c26f9e61b652fce3daf3',
+    '_ga': 'GA1.2.124877360.1648781979',
+    '_gid': 'GA1.2.1917683348.1648781979',
+    '_gat': '1',
+    'PHPSESSID': 'rs8rnrdrj725tto611iagujs11',
+    'wordpress_logged_in_f82bae77b4519a305f00e3f66136abf7': 'zq1121734772%7C1649991583%7CfV9buiUC9OrWiuxM2dUzyH4DYG5soU3p8xL8yqGZRgn%7Ccec4c0e1bfe1ff1b9de9574b58e350bbb3fbbce3e2c0132ab0cd60166b0fcf74',
+}
+
+headers = {
+    'authority': 'miuiver.com',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Microsoft Edge";v="99"',
+    'dnt': '1',
+    'sec-ch-ua-mobile': '?0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.55',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'accept': 'application/json, text/javascript, */*; q=0.01',
+    'x-requested-with': 'XMLHttpRequest',
+    'sec-ch-ua-platform': '"Windows"',
+    'origin': 'https://miuiver.com',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://miuiver.com/user-profile/',
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    # Requests sorts cookies= alphabetically
+    # 'cookie': 'wordpress_sec_f82bae77b4519a305f00e3f66136abf7=zq1121734772%7C1649991583%7CfV9buiUC9OrWiuxM2dUzyH4DYG5soU3p8xL8yqGZRgn%7C2566c6da3e2145278cf58137313b609e78281e756268c26f9e61b652fce3daf3; _ga=GA1.2.124877360.1648781979; _gid=GA1.2.1917683348.1648781979; _gat=1; PHPSESSID=rs8rnrdrj725tto611iagujs11; wordpress_logged_in_f82bae77b4519a305f00e3f66136abf7=zq1121734772%7C1649991583%7CfV9buiUC9OrWiuxM2dUzyH4DYG5soU3p8xL8yqGZRgn%7Ccec4c0e1bfe1ff1b9de9574b58e350bbb3fbbce3e2c0132ab0cd60166b0fcf74',
+}
+
+data = {
+    'action': 'epd_checkin',
+}
 def main():
-    headers = {
-        'authority': 'miuiver.com',
-        'sec-ch-ua': '" Not;A Brand";v="99", "Microsoft Edge";v="97", "Chromium";v="97"',
-        'dnt': '1',
-        'sec-ch-ua-mobile': '?0',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 Edg/97.0.1072.69',
-        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'accept': 'application/json, text/javascript, */*; q=0.01',
-        'x-requested-with': 'XMLHttpRequest',
-        'sec-ch-ua-platform': '"Windows"',
-        'origin': 'https://miuiver.com',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-dest': 'empty',
-        'referer': 'https://miuiver.com/user-profile/',
-        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-        'cookie': 'wordpress_sec_f82bae77b4519a305f00e3f66136abf7=zq1121734772%7C1648521042%7CFUNxTeWVbLkCP2FuOwuXHJS3ETIZVAq9loLjkUf3UsK%7C682070029882f581e43057823d2ccc17df258e0822f0f51a6b446b8274619ec1; _ga=GA1.2.1741035115.1646120001; _gid=GA1.2.612555610.1647311439; _gat=1; PHPSESSID=1j5kjcelkhv5n8fa0u5mab3qd0; wordpress_logged_in_f82bae77b4519a305f00e3f66136abf7=zq1121734772%7C1648521042%7CFUNxTeWVbLkCP2FuOwuXHJS3ETIZVAq9loLjkUf3UsK%7C72ffb67e721b3e38d2e942744c6c42b7ff86b12501b67209db4cae836c842d09',
-    }
-
-    data = {
-      'action': 'epd_checkin'
-    }
-
-    response = requests.post('https://miuiver.com/wp-admin/admin-ajax.php', headers=headers, data=data)
+    response = requests.post('https://miuiver.com/wp-admin/admin-ajax.php', headers=headers, cookies=cookies, data=data)
     print(response.json())
 
 if __name__=='__main__':
