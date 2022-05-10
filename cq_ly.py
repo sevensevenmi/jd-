869b288cc1be5462082abfa049a735b6d531b709
七mi <42993271+sevensevenmi@users.noreply.github.com>
2022-05-10 17:30:18 +0800

    Update cq_ly.py

--- cq_ly.py
+++ cq_ly.py
@@ -9,27 +9,30 @@ aDm8H%MdA
 cron:
 58 11,23 * * * cq_ly.py
 """
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 import urllib.request
+
+
 passwd = "1121734772"
 chrome_options = Options()
 chrome_options.add_argument('--headless') # 16年之后，chrome给出的解决办法，抢了PhantomJS饭碗
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')  # root用户不加这条会无法运行
-driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("http://192.168.68.1/")
-time.sleep(2)
+time.sleep(3)
 driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div/div[2]/form/div/div/div[1]/input").send_keys(passwd)
 driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div/div[2]/form/button").click()
-time.sleep(3)
+time.sleep(2)
 driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[1]/div[4]/div[2]/button/span/i").click()
 
-time.sleep(1)
-driver.find_element(By.XPATH, '/html/body/div[5]/div/div[3]/button[2]/span').click()
 time.sleep(2)
+driver.find_element(By.XPATH, '/html/body/div[5]/div/div[3]/button[2]/span').click()
+time.sleep(3)
 for i in range(5):
     print(driver.find_element(By.XPATH, '/html/body/div[6]/div/p').text)
 
@@ -39,6 +42,7 @@ t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 bip_4 = urllib.request.urlopen('http://4.ipw.cn/').read()
 #ip = str(bip, encoding="utf-8")
 ip = bytes.decode(bip_4)
+print(ip)
 with open("/ql/data/log/sevensevenmi_jd-_cq/cq_lyrecord.txt", 'a') as file_object:
     file_object.write(t)
     file_object.write('---重启成功---出口ip:')
